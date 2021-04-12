cf00151a0f389f77f1c09a6792a69aeb0ada46db
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2021-04-12 23:32:31 +0200

    Removed unnecesary import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './pages/list/list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { DetailComponent } from './pages/detail/detail.component';
 import { InfoCardComponent } from './components/info-card/info-card.component';
@@ -24,9 +23,7 @@ import { FormsModule } from '@angular/forms';
     MatCardModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     MatIconModule,
-    BrowserAnimationsModule,
     FormsModule
   ],
   providers: [],
